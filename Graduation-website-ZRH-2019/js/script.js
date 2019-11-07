f8a6395bbf15527d51d2b9e7b68a25b519a7cfa2
hasan.hawar@powercoders.org <mhdhasan.hawar@students.bfh.ch>
2019-11-07 10:29:26 +0100

    new projects

---
+++ Graduation-website-ZRH-2019/js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('#countdown').countdown({
+        year: 2019, // YYYY Format
+        month: 12, // 1-12
+        day: 6, // 1-31
+        hour: 0, // 24 hour format 0-23
+        minute: 0, // 0-59
+        second: 0, // 0-59
+    });
+});
---
+++ LastVersion/js/app.js
@@ -0,0 +1,7 @@
+new fullpage("#fullpage", {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ["Home", "About us", "Schedule", "Registration", "Location"],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+})
